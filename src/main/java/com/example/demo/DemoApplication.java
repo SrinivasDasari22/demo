f7028086f124c9e3cc10390951176934f4e7a953
SrinivasDasari22 <dsaikrishna333@gmail.com>
2023-04-07 23:44:08 +0000

    commit 2

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,7 +15,7 @@ public class DemoApplication {
 
 	@GetMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot!---- +Hey DS";
+		return "Greetings from Spring Boot!--  Hey DS";
 	}
 
 
